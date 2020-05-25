1f9409550291e36475fc3379b291a86b236924ca
uvsq21602357 <clement.carre@ens.uvsq.fr>
2020-05-25 18:22:24 +0200

    Lecture des graphes dans la structure

--- src/Lecture.c
+++ src/Lecture.c
@@ -1,9 +1,13 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "time.h"
+#include "structure.c"
+
+int NBSommets;
 
 
-void LectureFichier(char *Chemin){
+
+Liste* LectureFichier(char *Chemin){
 	FILE* fichier = NULL;
     char caractereActuel;
     int NombreArcs;
@@ -19,6 +23,7 @@ void LectureFichier(char *Chemin){
     {
 		fscanf(fichier, "%d", &NombreArcs);			//Lecture de la première valeur du fichier
 		fscanf(fichier, "%d", &NombreSommets);			//Nombre de sommets de la matrice
+		Liste* tab = initTableau(NombreSommets);
         printf("%d \n", NombreArcs);
         printf("%d \n", NombreSommets);
         
@@ -28,23 +33,32 @@ void LectureFichier(char *Chemin){
             printf("%d %d", NumeroLigne, NbArcsLigne);
             for(j = 0; j < NbArcsLigne; j++){
 				fscanf(fichier, "%d %lf", &Colonne, &Valeur);	//Recupération pour chaque arc de la ligne de sa destination et de sa colonne
+				Sommet *s = initSommet( NumeroLigne-1, Colonne-1, Valeur);
+				ajouterSommet(&tab[Colonne-1],s);
 				printf(" %d %lf", Colonne, Valeur);
 			}
 			printf("\n");
         } 
-     
+        
+     NBSommets = NombreSommets;
      fclose(fichier);   		//Fermeture du fichier
+     return tab;
     }
     else
     {
 		printf("Impossible d'ouvrir le graphe");
+		return 0;
     }
 }
 
 int main(){
 	int time = 0;
 	char *c = "../Graphe/web1.txt";
-	LectureFichier(c);
+	Liste* tab = LectureFichier(c);
+	printf("NSOMMETS = %d\n",NBSommets);
+	printf("\n\n------AffichageListe--------\n\n");
+	afficherTableau(tab,NBSommets);
+	//~ freeListe(tab);
 	time = clock();		//Temps d'execution du programme
 	printf("\033[1;32m");
     printf("Temps d'execution = %d ms\n", time);
