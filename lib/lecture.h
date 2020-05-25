1465cf170d0ce65f4d71d2a43034461eb3e3df4f
uvsq21602357 <clement.carre@ens.uvsq.fr>
2020-05-25 22:00:58 +0200

    Lecture.h

---
+++ lib/lecture.h
@@ -0,0 +1,24 @@
+#ifndef STRUCTURE_H
+#define STRUCTURE_H
+
+/*
+Get the number of arcs
+fichier : the graph file open
+*/
+int NBarcs(FILE* fichier);
+
+/*
+Get the number of summits
+fichier : the graph file open
+*/
+int NBsommets(FILE* fichier);
+
+/*
+Get the graph
+fichier : the graph file open
+NombreSommets : The number of summits
+*/
+Liste* LectureFichier(FILE* fichier, int NombreSommets);
+
+
+#endif
