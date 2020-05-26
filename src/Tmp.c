58b9bc51537bcdc623747451512dc0ef00fa42d8
uvsq21602357 <clement.carre@ens.uvsq.fr>
2020-05-26 13:02:10 +0200

    Correction avec les pointeurs

--- src/Tmp.c
+++ src/Tmp.c
@@ -2,11 +2,11 @@
 #include "stdio.h"
 #include "../lib/vector.h"
 
-double VxVt(vector v1, vector v2){
+double VxVt(VEC *v1, VEC *v2){
 	double res = 0;
 	int i;
-	for(i = 0; i < v1.size; i++){		//On parcours les vecteurs en entier
-		res = res + (v1[i] * v2[i]);
+	for(i = 0; i < v1->size; i++){		//On parcours les vecteurs en entier
+		res = res + (v1->array[1] * v2->array[2]);
 	}
 	printf("Res = %lf\n", res);
 	return res;
